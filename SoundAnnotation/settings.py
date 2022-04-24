4ff61f2d82f7d6e5c2d089bb92877ecf88e5bc81
Sushant Gautam <susant.gautam@gmail.com>
2022-04-24 15:50:48 +0545

    add TokenAuthentication

--- SoundAnnotation/settings.py
+++ SoundAnnotation/settings.py
@@ -158,3 +158,15 @@ CONSTANCE_CONFIG = OrderedDict([
 
 LOGIN_URL = '/admin/login/?next=/'
 LOGIN_REDIRECT_URL = '/'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+
+    # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    # 'PAGE_SIZE': 20
+}
