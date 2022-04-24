31ffae58b31eafdb8536ef9102b8c8bc07b1d895
Sushant Gautam <susant.gautam@gmail.com>
2022-04-24 20:18:54 +0545

    remove cors

--- SoundAnnotation/settings.py
+++ SoundAnnotation/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django_filters',
     'channels',
     'constance',
-    "corsheaders",
+    # "corsheaders",
     'constance.backends.database',
     'django_cleanup.apps.CleanupConfig',
 
@@ -174,5 +174,5 @@ REST_FRAMEWORK = {
 }
 
 
-CORS_ALLOW_ALL_ORIGINS = True # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
-CORS_ALLOW_CREDENTIALS = True
+# CORS_ALLOW_ALL_ORIGINS = True # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
+# CORS_ALLOW_CREDENTIALS = True
