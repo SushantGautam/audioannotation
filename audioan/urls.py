fe07511ff41654b005c080f971c35ccfd058540b
Sujil Devkota <sujil@nsdevil.com>
2022-05-06 12:25:58 +0545

    url setup for each app with homepage

--- audioan/urls.py
+++ audioan/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('orgadmin.urls')),
+    path('professor/', include('professor.urls')),
+    path('speaker/', include('speaker.urls')),
+    path('worker/', include('worker.urls')),
 ]
