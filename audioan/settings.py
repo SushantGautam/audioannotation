a6c23b71fd1c456dd6a8502a936f7165d31ad9f2
Sujil Devkota <sujil@nsdevil.com>
2022-05-06 15:57:05 +0545

    static files added

--- audioan/settings.py
+++ audioan/settings.py
@@ -149,7 +149,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = 'account_login'
 LOGIN_REDIRECT_URL = "/"
-LOGOUT_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "account_login"
 
 SITE_ID = 1
 
