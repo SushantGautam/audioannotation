42209782fcdcca1bbfd217f95cb172e7f6941156
Sujil Devkota <sujil@nsdevil.com>
2022-05-08 11:28:38 +0545

    UI update

--- old_code/SoundAnnotation/celeryy.py
+++
@@ -1,9 +0,0 @@
-import os
-
-from celery import Celery
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SoundAnnotation.settings')
-
-app = Celery('SoundAnnotation',)
-app.config_from_object('django.conf:settings', namespace='CELERY')
-app.autodiscover_tasks()
