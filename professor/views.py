9e130f0736b098a6acbae38d78bd846ba2e7a8ca
pawan <juneisbackagain@gmail.com>
2022-05-09 14:43:56 +0545

    professor Question List Page and datatable statics added to base_html

--- professor/views.py
+++ professor/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 
 def homepage(request):
     return render(request, 'professor/homepage.html')
+
+
+def QuestionListPage(request):
+    return render(request, 'professor/QuestionListPage.html')
+
+
+def QuestionSetPage(request):
+    return render(request, 'professor/QuestionSetPage.html')
