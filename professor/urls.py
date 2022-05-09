7689400703150118afb6931a685a1f1ad52667c5
pawan <juneisbackagain@gmail.com>
2022-05-09 14:43:56 +0545

    professor Question List Page and datatable statics added to base_html

--- professor/urls.py
+++ professor/urls.py
@@ -5,4 +5,6 @@ app_name = 'professor'
 
 urlpatterns = [
     path('', views.homepage, name='homepage'),
+    path('professor_question_list', views.QuestionListPage, name='question_list_page'),
+    path('professor_question_set', views.QuestionSetPage, name='question_set_page'),
 ]
