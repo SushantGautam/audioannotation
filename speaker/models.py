62074907c3d77318e3cac7dfff07da532b19bdf2
Sujil Devkota <sujil@nsdevil.com>
2022-05-11 22:04:59 +0545

    worker and spearker submission data added

--- speaker/models.py
+++ speaker/models.py
@@ -1,7 +1,6 @@
-from distutils.command.upload import upload
-import os
 from django.db import models
 from orgadmin.models import BaseUserModel
+from professor.models import Question
 
 class Speaker(BaseUserModel):
     class Meta:
@@ -15,9 +14,30 @@ def audio_filename(instance, filename):
 class AudioFile(models.Model):
     date_created = models.DateTimeField(auto_now_add=True)
     audio_file = models.FileField(upload_to=audio_filename)
-    speaker = models.ForeignKey(Speaker, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.audio_file.name
+
+class SpeakerSubmission(models.Model):
+    STATUS_CHOICES = (
+        ('IS', 'Initial State'),
+        ('SS', 'STT Submitted'),
+        ('SP', 'STT Processing'),
+        ('SC', 'STT Completed'),
+        ('SF', 'STT Failed'),
+    )
+
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    speaker = models.ForeignKey(Speaker, on_delete=models.CASCADE)
+    audio_file = models.FileField(upload_to=audio_filename)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+    stt_data = models.JSONField(null=True, blank=True, default=dict)
+    status = models.CharField(max_length=2, choices=STATUS_CHOICES, default='IN')
+
+    def __str__(self):
+        return self.question.title
+
 
 
--- worker/models.py
+++ worker/models.py
@@ -6,3 +6,11 @@ class Worker(BaseUserModel):
     class Meta:
         verbose_name = 'Worker'
         verbose_name_plural = 'Workers'
+
+class WorkerSubmission(models.Model):
+    worker = models.ForeignKey(Worker, on_delete=models.CASCADE)
+    speaker_submission = models.ForeignKey('speaker.SpeakerSubmission', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+    split_data = models.JSONField(null=True, blank=True, default=dict)
