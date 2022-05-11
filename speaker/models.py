876074676633059b953a1c5abb23566038d13a8b
Abhinab <abhinabacharya303@gmail.com>
2022-05-11 18:18:15 +0545

    record and save

--- speaker/models.py
+++ speaker/models.py
@@ -1,7 +1,27 @@
+import os
+
 from django.db import models
 from orgadmin.models import BaseUserModel
+
 
 class Speaker(BaseUserModel):
     class Meta:
         verbose_name = 'Speaker'
         verbose_name_plural = 'Speakers'
+
+
+def update_filename(instance, filename):
+    path = "upload/path/"
+    format = str(instance.pk) + "_new_audio.wav"
+    return os.path.join(path, format)
+
+
+class AudioFile(models.Model):
+    date_created = models.DateTimeField(auto_now_add=True)
+    audio_file = models.FileField(upload_to=update_filename)
+
+    class Meta:
+        verbose_name = 'AudioFile'
+        verbose_name_plural = 'AudioFiles'
+
+
--- speaker/views.py
+++ speaker/views.py
@@ -1,4 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.template import RequestContext
+from django.views.decorators.csrf import csrf_exempt, csrf_protect
 
+from speaker.forms import AudioFileForm
+
+
+@csrf_protect
 def homepage(request):
+    # return render(request, 'speaker/homepage.html')
     return render(request, 'speaker/homepage.html')
+
+
+def save_audio(request):
+    if request.method == 'POST':
+        form = AudioFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            print("File saved")
+            form.save()
+        else:
+            print("form invalid: ", form.errors)
+    return redirect('homepage')
