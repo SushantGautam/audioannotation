b3090f03e405810557e8e309d699d76746183081
Sujil Devkota <sujil@nsdevil.com>
2022-05-20 15:00:25 +0545

    speaker model update

--- speaker/models.py
+++ speaker/models.py
@@ -1,34 +1,39 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+
 from orgadmin.models import BaseUserModel
 from professor.models import Question
+from speaker.choices import GENDER_CHOICES, COUNTRY_CHOICES, LANGUAGE_CHOICES, PROFICIENCY_CHOICES
 
 
 class Speaker(BaseUserModel):
+    birth_date = models.DateField(null=True, blank=True)
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES, default='F')
+    nationality = models.CharField(max_length=2, choices=COUNTRY_CHOICES, default='KR')
+    language = models.CharField(max_length=2, choices=LANGUAGE_CHOICES, default='KO')
+    proficiency = models.CharField(max_length=1, choices=PROFICIENCY_CHOICES, default='B')
+    topic_score = models.IntegerField(default=0)
+    education_level = models.CharField(max_length=256, null=True, blank=True)
+    learning_period = models.IntegerField(default=0)
+    korea_residency = models.IntegerField(default=0)
+    study_purpose = models.CharField(max_length=256, null=True, blank=True)
+    learning_method = models.CharField(max_length=256, null=True, blank=True)
     class Meta:
-        verbose_name = 'Speaker'
-        verbose_name_plural = 'Speakers'
+        verbose_name = _('Speaker')
+        verbose_name_plural = _('Speakers')
 
 
 def audio_filename(instance, filename):
     return 'speaker/audio/{0}/{1}'.format(instance.speaker, str(instance.id) + filename)
 
 
-class AudioFile(models.Model):
-    date_created = models.DateTimeField(auto_now_add=True)
-    audio_file = models.FileField(upload_to=audio_filename)
-    speaker = models.ForeignKey(Speaker, on_delete=models.CASCADE, blank=True, null=True)
-
-    def __str__(self):
-        return self.audio_file.name
-
-
 class SpeakerSubmission(models.Model):
     STATUS_CHOICES = (
-        ('IS', 'Initial State'),
-        ('SS', 'STT Submitted'),
-        ('SP', 'STT Processing'),
-        ('SC', 'STT Completed'),
-        ('SF', 'STT Failed'),
+        ('IS', _('Initial State')),
+        ('SS', _('STT Submitted')),
+        ('SP', _('STT Processing')),
+        ('SC', _('STT Completed')),
+        ('SF', _('STT Failed')),
     )
 
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
