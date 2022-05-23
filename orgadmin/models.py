4cc1ba6dec020088f82a2673c713e5579b364917
Sujil Devkota <sujil@nsdevil.com>
2022-05-23 15:39:58 +0545

    organization model added

--- orgadmin/models.py
+++ orgadmin/models.py
@@ -1,10 +1,22 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class Organization(models.Model):
+    name = models.CharField(max_length=256)
+    address = models.CharField(max_length=256, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
 class BaseUserModel(models.Model):
     user = models.OneToOneField('auth.User', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    organization_code = models.ForeignKey(Organization, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.user.username
