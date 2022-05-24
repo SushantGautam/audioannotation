5a166a4b2a47aa10558c2b8dfe8deead21f777b0
pawan <juneisbackagain@gmail.com>
2022-05-24 12:37:47 +0545

    QuestionSet List Page and View Added

--- professor/urls.py
+++ professor/urls.py
@@ -11,6 +11,8 @@ urlpatterns = [
     path('professor_question/<int:pk>/update', views.QuestionsUpdateView.as_view(), name='question_update'),
     path('professor_question/<int:pk>/detail', views.QuestionDetailView.as_view(), name='question_detail'),
     path('professor_question/<int:pk>/delete', views.QuestionDeleteView, name='question_delete'),
+    path('professor_question/<int:pk>/delete/question_set', views.QuestionSetDeleteView, name='questionset_delete'),
     path('professor_question/delete_multiple_question', views.MultipleQuestionDeleteView, name='delete_multiple_question'),
-    path('professor_question_set', views.QuestionSetPage, name='question_set_page'),
+    path('professor_question/delete_multiple_question_set', views.MultipleQuestionSetDeleteView, name='delete_multiple_questionset'),
+    path('professor_question_set', views.QuestionSetListView.as_view(), name='question_set_page'),
 ]
