f1c91425655c00f5ec3f13f26da7a51163e05846
Sujil Devkota <sujil@nsdevil.com>
2022-05-27 16:01:35 +0545

    import export added for question model

--- professor/admin.py
+++ professor/admin.py
@@ -1,9 +1,20 @@
 from django.contrib import admin
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
+
 from .models import *
 
 admin.site.register(Professor)
 admin.site.register(Category)
 admin.site.register(SubCategory)
-admin.site.register(Question)
+
+class QuestionResource(resources.ModelResource):
+    class Meta:
+        model = Question
+
+class QuestionAdmin(ImportExportModelAdmin):
+    resource_class = QuestionResource
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(QuestionSet)
 admin.site.register(ExamSet)
