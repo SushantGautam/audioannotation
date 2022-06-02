6af6d42d766367a34bde7ed6080b9c8e42bba0f9
Sujil Devkota <sujil@nsdevil.com>
2022-06-02 15:34:32 +0545

    exam set and questionset import export added

--- professor/admin.py
+++ professor/admin.py
@@ -15,6 +15,20 @@ class QuestionResource(resources.ModelResource):
 class QuestionAdmin(ImportExportModelAdmin):
     resource_class = QuestionResource
 
+class QuestionSetResource(resources.ModelResource):
+    class Meta:
+        model = QuestionSet
+
+class QuestionSetAdmin(ImportExportModelAdmin):
+    resource_class = QuestionSetResource
+
+class ExamSetResource(resources.ModelResource):
+    class Meta:
+        model = ExamSet
+
+class ExamSetAdmin(ImportExportModelAdmin):
+    resource_class = ExamSetResource
+
 admin.site.register(Question, QuestionAdmin)
-admin.site.register(QuestionSet)
-admin.site.register(ExamSet)
+admin.site.register(QuestionSet, QuestionSetAdmin)
+admin.site.register(ExamSet, ExamSetAdmin)
