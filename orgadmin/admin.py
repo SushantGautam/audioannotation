da0fa094718e4bcfee1d5aa820ea9a12b507c872
Sujil Devkota <sujil@nsdevil.com>
2022-06-03 16:31:18 +0545

    verification request added in admin

--- orgadmin/admin.py
+++ orgadmin/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import OrgAdmin, Organization, Contract, ContractSign
+from .models import OrgAdmin, Organization, Contract, ContractSign, VerificationRequest
 
 admin.site.register(OrgAdmin)
 admin.site.register(Organization)
 admin.site.register(Contract)
 admin.site.register(ContractSign)
+admin.site.register(VerificationRequest)
 
